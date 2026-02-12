9ae4e8ccf15c7b98f11042b09f9f82db28d6b412
Laurent Pugin <lxpugin@gmail.com>
2026-02-12 08:24:22 +0100

    change indent

--- src/components/TreeNode.svelte
+++ src/components/TreeNode.svelte
@@ -1,55 +1,50 @@
 <script lang="ts">
-  import { createEventDispatcher } from "svelte";
-  import type { TreeNodeData } from "../app/types";
+    import { createEventDispatcher } from "svelte";
+    import type { TreeNodeData } from "../app/types";
 
-  export let node: TreeNodeData;
-  export let isRoot = false;
-  export let selectedId: string | null = null;
+    export let node: TreeNodeData;
+    export let isRoot = false;
+    export let selectedId: string | null = null;
 
-  const dispatch = createEventDispatcher<{
-    select: string;
-    hover: string | null;
-  }>();
+    const dispatch = createEventDispatcher<{
+        select: string;
+        hover: string | null;
+    }>();
 
-  function handleSelect() {
-    dispatch("select", node.id);
-  }
+    function handleSelect() {
+        dispatch("select", node.id);
+    }
 
-  function handleMouseEnter() {
-    dispatch("hover", node.id);
-  }
+    function handleMouseEnter() {
+        dispatch("hover", node.id);
+    }
 
-  function handleMouseLeave() {
-    dispatch("hover", null);
-  }
+    function handleMouseLeave() {
+        dispatch("hover", null);
+    }
 </script>
 
 <div
-  class={isRoot ? "vrv-tree-root open" : `vrv-tree-node${node.isLeaf ? " leaf" : ""}`}
-  data-id={node.id}
-  data-element={node.element}
->
-  <div
-    class="vrv-mei-element vrv-node-label {node.id === selectedId ? 'target checked' : ''}"
+    class={isRoot ? "vrv-tree-root open" : `vrv-tree-node${node.isLeaf ? " leaf" : ""}`}
     data-id={node.id}
     data-element={node.element}
-    style={isRoot ? "display: none;" : undefined}
-    on:click|stopPropagation={handleSelect}
-    on:mouseenter={handleMouseEnter}
-    on:mouseleave={handleMouseLeave}
-  >
-    {node.element}
-  </div>
-  <div class="vrv-node-children">
-    {#if node.children?.length}
-      {#each node.children as child}
-        <svelte:self
-          node={child}
-          {selectedId}
-          on:select
-          on:hover
-        />
-      {/each}
-    {/if}
-  </div>
+>
+    <div
+        class="vrv-mei-element vrv-node-label {node.id === selectedId ? 'target checked' : ''}"
+        data-id={node.id}
+        data-element={node.element}
+        style={isRoot ? "display: none;" : undefined}
+        on:click|stopPropagation={handleSelect}
+        on:mouseenter={handleMouseEnter}
+        on:mouseleave={handleMouseLeave}
+    >
+        {node.element}
+    </div>
+    <div class="vrv-node-children">
+        {#if node.children?.length}
+            {#each node.children as child}
+                <svelte:self node={child} {selectedId} on:select on:hover />
+            {/each}
+        {/if}
+    </div>
 </div>
